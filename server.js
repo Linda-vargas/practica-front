ae5468764b08a61dc72dfebb92d1359c969296db
Linda-vargas <linda.vargas@unmsm.edu.pe>
2021-11-11 02:24:51 -0500

    package

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
-const express = require('express')
-const path = require('path')
-
+const path = require('path');
+const express = require('express');
 const app = express();
 
-app.use(express.static('./dist/practica'))
+// Serve static files
+app.use(express.static(__dirname + '/dist/practica'));
 
-app.get('/*',(req,res)=>
-res.sendFile('index.html',{root:'dist/practica/'}))
+// Send all requests to index.html
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname + '/dist/practica/index.html'));
+});
 
-app.listen(precess.env.PORT || 8080)
+// default Heroku port
+app.listen(process.env.PORT || 8080);
