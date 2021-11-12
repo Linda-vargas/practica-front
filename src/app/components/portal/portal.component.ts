ea423dd8fa3c7c024c118eb23e35aed63fc743d1
Linda-vargas <linda.vargas@unmsm.edu.pe>
2021-11-12 00:11:15 -0500

    fin

--- src/app/components/portal/portal.component.ts
+++ src/app/components/portal/portal.component.ts
@@ -1,5 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
 
 @Component({
   selector: 'app-portal',
@@ -13,6 +15,9 @@ export class PortalComponent implements OnInit {
   sumnota:number=0;
   sumcredito:number=0;
   promedio:number=0;
+  mensaje:any
+  isError:boolean=false
+  isLoad:boolean=false
   constructor(private http: HttpClient) { }
   
   ngOnInit(): void {
@@ -20,16 +25,44 @@ export class PortalComponent implements OnInit {
 
 
   obtenerDatos(){
-    this.http.get<any>('https://practicaintegracion.herokuapp.com/pc/listar_alumnos?codalu='+this.codigo).subscribe(x=>{
-      this.data=x
-      this.enabled=true
-      this.data.curso.forEach((e:any) => {
-        this.sumnota= e.nota*e.credito
-        this.sumcredito=e.credito
-        
-      });
-      this.promedio=this.sumnota/this.sumcredito
-    });
+    this.promedio=0;
+    this.sumnota=0
+    this.sumcredito=0
+    this.isError=false
+    this.isLoad=true
+    this.api().subscribe(x=>{
+
+        this.data=x
+        if(this.data.curso.length===0){
+          this.promedio=0
+          this.mensaje="El alumno no ha sido matriculado en ningun curso"
+        this.isError=true
+        this.enabled=true
+        this.isLoad=false
+        }else{
+        this.enabled=true
+        this.data.curso.forEach((e:any) => {
+          this.sumnota= e.nota*e.credito
+          this.sumcredito=e.credito
+        });
+        this.promedio=this.sumnota/this.sumcredito
+        this.isLoad=false
+      }
+      
+    
+    })
+  }
+
+  api(){
+    return this.http.get<any>('https://practicaintegracion.herokuapp.com/pc/listar_alumnos?codalu='+this.codigo).pipe(
+      catchError((e:any)=>{
+        this.mensaje="El código no existe"
+        this.isError=true
+        this.isLoad=false
+        this.enabled=false
+        return throwError(e);
+      })
+    );
   }
 
 }
